7fbd0a6c3b8fb6432951bad0b72e5e2b0f40f6b4
Hyovin Kwak <hyovin.kwak@tu-dortmund.de>
2025-12-04 12:44:34 +0100

    custom.el added

---
+++ custom.el
@@ -0,0 +1,41 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("c0dd134ecd6ede6508c30f7d4ac92334229531df62284fc6572f65b4d0cde43f"
+     "0f1341c0096825b1e5d8f2ed90996025a0d013a0978677956a9e61408fcd2c77"
+     "f253a920e076213277eb4cbbdf3ef2062e018016018a941df6931b995c6ff6f6"
+     "b7a09eb77a1e9b98cafba8ef1bd58871f91958538f6671b22976ea38c2580755"
+     "e1df746a4fa8ab920aafb96c39cd0ab0f1bac558eff34532f453bd32c687b9d6"
+     "b99ff6bfa13f0273ff8d0d0fd17cc44fab71dfdc293c7a8528280e690f084ef0"
+     "2ab8cb6d21d3aa5b821fa638c118892049796d693d1e6cd88cb0d3d7c3ed07fc"
+     "7ec8fd456c0c117c99e3a3b16aaf09ed3fb91879f6601b1ea0eeaee9c6def5d9"
+     "8d3ef5ff6273f2a552152c7febc40eabca26bae05bd12bc85062e2dc224cde9a"
+     "6e692e902614c1be82032a986a227d83d05278390ebfeb4fe0cc6e718e6b6b08"
+     "5244ba0273a952a536e07abaad1fdf7c90d7ebb3647f36269c23bfd1cf20b0b8"
+     "5c7720c63b729140ed88cf35413f36c728ab7c70f8cd8422d9ee1cedeb618de5"
+     "166a2faa9dc5b5b3359f7a31a09127ebf7a7926562710367086fcc8fc72145da" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(centaur-tabs-selected ((t (:background "#eaeef2" :foreground "#0f66c8" :weight bold))))
+ '(centaur-tabs-selected-modified ((t (:background "#eaeef2" :foreground "#0f66c8" :weight bold))))
+ '(centaur-tabs-unselected ((t (:background "#eaeef2" :foreground "#888888"))))
+ '(centaur-tabs-unselected-modified ((t (:background "#eaeef2" :foreground "#888888"))))
+ '(doom-modeline-buffer-modified ((t (:foreground "#666666" :weight bold))))
+ '(doom-modeline-buffer-path ((t (:foreground "#00aa00" :weight bold))))
+ '(doom-modeline-project-dir ((t (:foreground "#0000aa" :weight bold))))
+ '(doom-modeline-warning ((t (:foreground "#aa0000" :weight bold))))
+ '(font-lock-string-face ((t (:foreground "#00AA00"))))
+ '(header-line ((t (:foreground "#383a42" :weight bold))))
+ '(hl-line ((t (:background "#e2e6ec" :underline nil))))
+ '(lsp-face-highlight-read ((t (:inherit nil :background "dark sea green" :underline nil :weight bold))))
+ '(lsp-face-highlight-textual ((t (:inherit nil :background "dark sea green" :underline nil :weight bold))))
+ '(lsp-face-highlight-write ((t (:inherit nil :background "dark sea green" :underline nil :weight bold))))
+ '(region ((t (:background "#4671d5" :foreground "#ffffff"))))
+ '(show-paren-match ((t (:foreground "#e45649" :background "#f0f0f0" :weight bold)))))
