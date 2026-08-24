cb1473f874013eaf41910e9ee2632ee6d1660905
Hyovin Kwak <hyovin.kwak@th-koeln.de>
2026-08-24 09:15:49 +0200

    init-org update

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -27,15 +27,15 @@
   ;;; Set org level colors
   (setq org-n-level-faces 8)
   (custom-set-faces!
-    ;; High-contrast multi-color palette tuned for yellow/cream backgrounds
-    '(org-level-1 :foreground "#B91C1C" :weight bold :height 1.45) ; Crimson Red
-    '(org-level-2 :foreground "#C2410C" :weight bold :height 1.40) ; Burnt Orange
-    '(org-level-3 :foreground "#6B21A8" :weight bold :height 1.35)             ; Deep Purple
-    '(org-level-4 :foreground "#1D4ED8" :weight bold :height 1.30)             ; Strong Royal Blue
-    '(org-level-5 :foreground "#047857" :weight bold :height 1.25)             ; Deep Emerald Green
-    '(org-level-6 :foreground "#0F766E" :weight bold :height 1.20)             ; Dark Teal
-    '(org-level-7 :foreground "#431407" :weight bold :height 1.15)             ; Dark Chocolate Brown
-    '(org-level-8 :foreground "#374151" :weight bold :height 1.1))            ; Slate Dark Gray
+    ;; High-contrast multi-color palett tuned for yellow/cream backgrounds
+    '(org-level-1 :foreground "#B91C1C") ; Crimson Red
+    '(org-level-2 :foreground "#C2410C") ; Burnt Orange
+    '(org-level-3 :foreground "#6B21A8")             ; Deep Purple
+    '(org-level-4 :foreground "#1D4ED8")             ; Strong Royal Blue
+    '(org-level-5 :foreground "#047857")             ; Deep Emerald Green
+    '(org-level-6 :foreground "#0F766E")             ; Dark Teal
+    '(org-level-7 :foreground "#431407")             ; Dark Chocolate Brown
+    '(org-level-8 :foreground "#374151"))            ; Slate Dark Gray
 
   ;; If you use `org' and don't want your org files in the default location below,
   ;; change `org-directory'. It must be set before org loads!
@@ -45,7 +45,7 @@
   (setq org-startup-folded 'overview)
 
   ;; enable font-lock for bold/italic/underline/etc.
-  (setq org-fontify-emphasized-text t)
+  ;; (setq org-fontify-emphasized-text t)
   (setq org-hide-emphasis-markers t)
 
   ;; (optional) also prettify symbols like ->, <=, etc.
